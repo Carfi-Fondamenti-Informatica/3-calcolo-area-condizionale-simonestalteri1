0f57b9e660172db75f3dc01ee12fc1ab5cdc4bc7
simonestalteri1 <117160924+simonestalteri1@users.noreply.github.com>
2022-11-17 20:05:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 using namespace std;
+int main()
+{
+float a,b;
+int opzione;
+cin>> a >> b >> opzione ;
+switch(opzione)
+{
+    case 0:
+        opzione= ((a*b)/2);
+        cout<< (a*b)/2<< endl;
+        break;
 
-int main() {
-   return 0;
+    case 1:
+        opzione= (a*a);
+        cout<<(a*a)<<endl;
+        break;
+
+    case 2:
+        opzione= (a*b);
+        cout<<(a*b)<<endl;
+        break;
+
+    default:
+        cout<< "opzione non valida" << endl;
+}
+    return 0;
 }
